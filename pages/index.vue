12c04e8036127ab2b2e758b2c4e2a2c58096a12d
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-02-23 17:48:00 +0900

    Change file position

--- pages/index.vue
+++ pages/index.vue
@@ -24,7 +24,7 @@
 </template>
 
 <script>
-  import PostPreview from "../components/PostPreview";
+  import PostPreview from "../components/posts/PostPreview";
   export default {
     components: {PostPreview}
   }
