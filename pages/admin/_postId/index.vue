10bc7120d3ce720682187260ffbc5a1c7d633549
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-02-28 23:25:17 +0900

    feat: updating post function

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="admin-post-page">
     <section class="update-form">
-      <AdminPostForm :post="loadedPost" />
+      <AdminPostForm :post="loadedPost" @submit="onSubmitted" />
     </section>
   </div>
 </template>
@@ -15,13 +15,23 @@
     layout: "admin",
     components: {AdminPostForm},
     asyncData(context) {
-      return axios.get('https://my-nuxt-blog-691f1.firebaseio.com/posts/' + context.params.id + '.json')
+      return axios.get('https://my-nuxt-blog-691f1.firebaseio.com/posts/' + context.params.postId + '.json')
         .then(res => {
+          console.log('get post data');
           return {
             loadedPost: res.data
           };
         })
         .catch(e => context.error(e));
+    },
+    methods: {
+      onSubmitted(editedPost) {
+        axios.put('https://my-nuxt-blog-691f1.firebaseio.com/posts/' +
+          this.$route.params.postId +
+          '.json', editedPost)
+          .then(res => this.$router.push('/admin'))
+          .catch(e => console.log(e));
+      }
     }
   }
 </script>
